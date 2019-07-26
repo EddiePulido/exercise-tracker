b7f5f7c7b7ab6bbef0a8d3ede5f40d6bdefb1942
Eddie <genaroepulido@gmail.com>
2019-07-26 13:56:06 -0700

    changed server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require ('mongoose');
 
+const path = require("path");
+
+
 // require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "../public")))
 
 app.use(cors());
 app.use(express.json());
