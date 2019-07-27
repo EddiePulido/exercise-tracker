b426d06b1449fe2f3c8d9347b78136dcf6bbfa75
Eddie <genaroepulido@gmail.com>
2019-07-26 17:00:15 -0700

    added build

---
+++ client/build/precache-manifest.8ac4ac1d8f31290ff4bc9dfec224d526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29b2aa436f8c3b1110ded4fc358ec8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b98b749ee534a6c7c121",
+    "url": "/static/css/2.4ed46a55.chunk.css"
+  },
+  {
+    "revision": "ab4805c284a5d27e3228",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b98b749ee534a6c7c121",
+    "url": "/static/js/2.f9532b76.chunk.js"
+  },
+  {
+    "revision": "ab4805c284a5d27e3228",
+    "url": "/static/js/main.ba6c6f08.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
